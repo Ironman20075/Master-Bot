8b30e5b2372037d5bf7782ddb469577a89dc8df9
Ironman20075 <67406020+Ironman20075@users.noreply.github.com>
2020-07-23 13:04:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
   "token": "NzM1NTU0NTI0MTg5ODE4OTYw.Xxlaeg.3Nqy4Z_mIHk8RcOqtB-5uaYm8xA",
   "tenorAPI": "FD3TOCOABI9B",
   "newsAPI": "8e569d5c9f1c42efaae974a08f72393a",
-  "youtubeAPI": "youtube-api-key",
+  "youtubeAPI": "AIzaSyB08gV-L2YIjyvbjLrge8mPiI5PlU23WAM",
   "yandexAPI": "yandex-api-key",
-  "geniusLyricsAPI": "genius-api-key"
+  "geniusLyricsAPI": "hLRqlsnjPeF9xgP2ShlY653BtzyUpCx0bIj_sqVqq67zU7FKFoGhi_5fcaaW1tbe"
