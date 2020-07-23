8775d3214c32f698bd6a6ae382b347f1aa3db8f7
Ironman20075 <67406020+Ironman20075@users.noreply.github.com>
2020-07-23 12:06:29 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+  "prefix": "!",
+  "discord_owner_id": "735550526909055057",
+  "token": "NzM1NTU0NTI0MTg5ODE4OTYw.Xxlaeg.3Nqy4Z_mIHk8RcOqtB-5uaYm8xA",
+  "tenorAPI": "FD3TOCOABI9B",
+  "newsAPI": "8e569d5c9f1c42efaae974a08f72393a",
+  "youtubeAPI": "youtube-api-key",
+  "yandexAPI": "yandex-api-key",
+  "geniusLyricsAPI": "genius-api-key"
